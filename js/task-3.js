f320e602066e6a12930b8c726e8b328942232f07
Bohdan-Palokha <palohabogdan21@gmail.com>
2024-03-18 17:20:51 +0200

    save

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,13 @@
 function getElementWidth(content, padding, border) {
+    content = Number.parseInt(content)
+    padding = Number.parseFloat(padding)
+    border = Number.parseInt(border)
     const total = (content + (padding * 2) + (border * 2));
-    const alert = `${total}px`;
 
-    return alert;
+    return total;
 };
-console.log(getElementWidth(50, 5, 2));
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+
